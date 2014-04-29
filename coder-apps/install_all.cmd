b0fde3eb86f10d5484ede166812c7606979add6f
Macon Pegram <mpegram@captechconsulting.com>
2014-04-29 09:43:50 -0400

    One script to rule them all.

---
+++ coder-apps/install_all.cmd
@@ -0,0 +1,15 @@
+@echo off
+if =%1-==-- echo "Usage: install_all.cmd [coderbase]" & exit /b
+
+set base=%1
+
+REM Create base folder
+IF NOT EXIST %base%   (
+   mkdir %base%
+)
+
+echo "Copying base files...."
+xcopy ..\common-base\*.* %base% /E
+
+echo "Installing common files"
+call install_common.cmd %base%
---
+++ coder-apps/install_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## 
+## Copies the common platform apps to
+## the coder-base working directory. 
+##
+## sh install_all base_path
+##
+## Eg.
+## sh install_all ../coder-base/
+
+if [ $# != 1 ]
+  then
+    echo -e "\nUse:\ninstall_all coderbase\n"
+    exit
+fi
+
+base=$1
+
+mkdir -p $base
+cp ../coder-base/* $base/
+./install_common.sh $base
